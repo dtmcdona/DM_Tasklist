2bf31060692361c5d99f447d3efad48c310975d0
dtmcdona <davidmcdwork@gmail.com>
2023-01-29 09:20:15 -0700

    Added RedisJSON database

--- app/core/redis_cache.py
+++ app/core/redis_cache.py
@@ -1,20 +1,47 @@
 """
-Currently, redis is only used for storing and retrieving the result of
-celery workers.  This is mainly used by the Task Manager to offload
-more expensive image processing to celery workers.
+Redis is only used for storing and retrieving the result of
+celery workers and caching the action collection since it is used
+frequently.  This is mainly used by the Task Manager to offload
+more expensive image processing to celery workers and allow some
+endpoints to be async.
 """
 from typing import Optional
 
 import redis
+from redis.commands.json.path import Path
 
 
 rc = redis.Redis(host="redis", port=6379, db=0)
 
-
-def set_condition_result(key, result) -> None:
+def set_condition_result(key: str, result: bool) -> None:
     rc.set(key, str(result))
 
 
-def get_condition_result(key) -> Optional[bool]:
-    cached_value = rc.get(str(key))
+def get_condition_result(key: str) -> Optional[bool]:
+    cached_value = rc.get(key)
     return eval(cached_value.decode("utf-8")) if cached_value else None
+
+
+def set_json(json_type: str, obj_id: str, json_dict: dict) -> None:
+    json_cache = {
+        json_type: json_dict
+    }
+    rc.json().set(f"{json_type}:{obj_id}", Path.root_path(), json_cache)
+
+
+def get_json(json_type: str, obj_id: str) -> dict:
+    cached_value = None
+    try:
+        cache_dict = rc.json().get(f"{json_type}:{obj_id}")
+        cached_value = cache_dict.get(json_type)
+    except Exception as e:
+        print(e)
+
+    return cached_value
+
+
+def del_json(json_type: str, obj_id: str) -> None:
+    try:
+        rc.json().delete(f"{json_type}:{obj_id}", Path.root_path())
+    except Exception as e:
+        print(e)
--- app/tests/test_redis_cache.py
+++ app/tests/test_redis_cache.py
@@ -5,15 +5,40 @@ from core import redis_cache
 
 class TestRedisCache:
     def test_redis_set_and_get(self):
-        key_name = f"test_key-{uuid4}"
-        redis_cache.set_condition_result(key_name, False)
-        res = redis_cache.get_condition_result(key_name)
+        key = f"test_key-{uuid4()}"
+        redis_cache.set_condition_result(key, False)
+        res = redis_cache.get_condition_result(key)
         assert res is False
-        key_name = f"test_key-{uuid4}"
-        redis_cache.set_condition_result(key_name, True)
-        res = redis_cache.get_condition_result(key_name)
+        redis_cache.set_condition_result(key, True)
+        res = redis_cache.get_condition_result(key)
         assert res is True
 
     def test_redis_key_dne(self):
         res = redis_cache.get_condition_result("test_key_dne")
         assert res is None
+
+    def test_json_set_and_get(self):
+        json_dict = {
+            "str": "str",
+            "int": 1,
+            "bool": True,
+            "str_array": ["str1", "str2"],
+            "int_array": [1, 2],
+            "bool_array": [True, False],
+            "nested_dict": {
+                "0": "str",
+                "1": 1,
+                "2": False,
+            }
+        }
+        json_type = "json_type_test"
+        id = str(uuid4())
+        redis_cache.set_json(json_type, id, json_dict)
+        res = redis_cache.get_json(json_type, id)
+        assert res == json_dict
+
+    def test_get_json_dne(self):
+        json_type = f"dne_test"
+        index = 0
+        assert not redis_cache.get_json(json_type, index)
+
